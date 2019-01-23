d93554648cd05d2a36eb6600573a71b792eca4c4
Jeremy Parra <jeremydeanparra@gmail.com>
2019-01-23 15:36:12 -0700

    file creation etc.

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mc Biggie", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Whooper", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The McDowell's", FALSE);
